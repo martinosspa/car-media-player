7ea7fb39c5b9f42e6ce465a851c5349a86e7f878
martinosspa <35999764+martinosspa@users.noreply.github.com>
2023-05-18 11:50:11 +0300

    removed unecessary imports from main ui

--- car-media-player/gui.py
+++ car-media-player/gui.py
@@ -1,6 +1,5 @@
-from kivy.core.window import Window
 from kivy.app import App
-from kivy.uix.image import Image
+from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
